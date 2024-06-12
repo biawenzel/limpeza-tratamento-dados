d09e44ffbe046fecc9eba304906bd351339083aa
Beatriz Wenzel <biahwenzel@gmail.com>
2024-06-12 15:07:26 -0300

    removing or filling null

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# limpeza-tratamento-dados
-Treinando limpeza e tratamento de dados usando Python para machine learning
+# Pandas: limpeza e tratamento de dados
+
+O curso de limpeza e tratamento de dados tem 5 módulos:
+1. Conhecendo os dados
+2. Transformação inicial dos dados
+3. Utilizando dados duplicados e nulos
+4. Lidando com os outliers
+5. Trabalhando com variáveis categóricas
+
+Neste curso, trabalhei com uma base de dados em JSON que usei para o seguinte passo a passo:
+* carregar/normalizar um json
+* preencher espaços em branco
+* converter os tipos errados de dados
+* identificar e tratar dados duplicados
+* lidar com dados nulos (substituindo ou excluindo)
--- tratamento.py
+++ tratamento.py
@@ -1,8 +1,8 @@
 import pandas as pd
-import json
+import json, numpy as np
 
 #carregando e normalizando json
-with open('Pandas - Limpeza e tratamento de dados/dataset-telecon.json') as df:
+with open('Pandas - Limpeza e tratamento de dados\limpeza-tratamento-dados\dataset-telecon.json') as df:
     df_json = json.load(df)
 
 df_normalizado = pd.json_normalize(df_json)
@@ -19,12 +19,33 @@ df_normalizado.loc[idx, 'cliente.tempo_servico'] = 24
 df_normalizado['conta.cobranca.Total'] = df_normalizado['conta.cobranca.Total'].astype(float)
 
 #identificando strings vazias
-for col in df_normalizado.columns:
+'''for col in df_normalizado.columns:
     print(f'Coluna: {col}')
     print(df_normalizado[col].unique())
-    print('-' * 30)
+    print('-' * 30)'''
 
 #criando uma cópia do df sem as linhas com string vazia em 'Churn'
 df_sem_vazio = df_normalizado[df_normalizado['Churn'] != ''].copy()
 df_sem_vazio.reset_index(drop=True, inplace=True)
-print(df_sem_vazio.info())
+
+#identificando e tratando dados duplicados
+valores_duplicados = df_sem_vazio.duplicated()
+df_sem_vazio.drop_duplicates(inplace=True)
+
+#identificando e substituindo dados nulos
+filtro = df_sem_vazio['cliente.tempo_servico'].isna()
+
+df_sem_vazio['cliente.tempo_servico'].fillna(
+    np.ceil(df_sem_vazio['conta.cobranca.Total'] / df_sem_vazio['conta.cobranca.mensal']), inplace=True
+) #preenchendo o tempo de serviço
+
+'''print(df_sem_vazio[filtro][['cliente.tempo_servico', 'conta.cobranca.mensal', 'conta.cobranca.Total']])'''
+
+#apagando os dados nulos
+colunas_dados_vazios = ['conta.contrato', 'conta.faturamente_eletronico', 'conta.metodo_pagamento']
+df_sem_nulos = df_sem_vazio.dropna(subset=colunas_dados_vazios)
+
+#resetando o index do df
+df_sem_nulos.reset_index(drop=True, inplace=True)
+
+#print(df_sem_vazio.info())
